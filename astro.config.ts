dade6dd05c36038a19bfaf3cad44eae6b2bf42c4
Duy Hung Nguyen <nd.hung@me.com>
2024-04-18 16:47:19 +0200

    s

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless';
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +14,9 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
+    output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
   ],
 })
